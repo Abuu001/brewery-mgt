3aeb5efd586c19d3ecf676061886e1eb33270028
Abuu001 <abulugz@gmail.com>
2021-07-16 00:10:58 +0300

    md

--- mssc-beer-service/src/main/java/com/lugonzo/msscbeerservice/repository/BeerRepository.java
+++ mssc-beer-service/src/main/java/com/lugonzo/msscbeerservice/repository/BeerRepository.java
@@ -1,11 +1,11 @@
 package com.lugonzo.msscbeerservice.repository;
 
-import com.lugonzo.msscbeerservice.domain.Beer;
+import com.lugonzo.msscbeerservice.model.BeerDto;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends PagingAndSortingRepository<BeerDto, UUID> {
 }
