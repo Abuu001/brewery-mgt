5039baa8de21e095e5ee25a5ab38d8a7d2097ea3
Abuu001 <abulugz@gmail.com>
2021-07-16 22:16:23 +0300

    set a dummy data to run when prog starts

--- mssc-beer-service/src/main/java/com/lugonzo/msscbeerservice/bootstrap/BeerLoader.java
+++ mssc-beer-service/src/main/java/com/lugonzo/msscbeerservice/bootstrap/BeerLoader.java
@@ -1,6 +1,8 @@
 package com.lugonzo.msscbeerservice.bootstrap;
 
 import com.lugonzo.msscbeerservice.domain.Beer;
+import com.lugonzo.msscbeerservice.model.BeerDto;
+import com.lugonzo.msscbeerservice.model.BeerStyleEnum;
 import com.lugonzo.msscbeerservice.repository.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,6 +11,10 @@ import java.math.BigDecimal;
 
 @Component
 public class BeerLoader implements CommandLineRunner {
+
+    public static final String BEER_1_UPC="01378238136137";
+    public static final String BEER_2_UPC="08131731733137";
+    public static final String BEER_3_UPC="05131371381378";
 
     private final BeerRepository beerRepository;
 
@@ -23,21 +29,24 @@ public class BeerLoader implements CommandLineRunner {
 
     private void loadBeerObjects(){
         if(beerRepository.count() == 0){
-            beerRepository.save(Beer.builder()
+            beerRepository.save(BeerDto.builder()
                    .beerName("Mongo pops")
-                    .beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(12813789127312L)
+                    .beerStyle(BeerStyleEnum.IPA)
+                    .upc(BEER_1_UPC)
                     .price(new BigDecimal("12.95"))
                     .build());
 
-            beerRepository.save(Beer.builder()
+            beerRepository.save(BeerDto.builder()
                     .beerName("Galaxy cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(12813789127313L)
+                    .beerStyle(BeerStyleEnum.GOSE)
+                    .upc(BEER_2_UPC)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+
+            beerRepository.save(BeerDto.builder()
+                    .beerName("Smirnof")
+                    .beerStyle(BeerStyleEnum.PALE_ALE)
+                    .upc(BEER_3_UPC)
                     .price(new BigDecimal("11.95"))
                     .build());
 
