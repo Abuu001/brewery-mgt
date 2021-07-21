39f6065199c78745c605f89bbf35a8ba8883e4b3
Abuu001 <abulugz@gmail.com>
2021-07-21 21:53:06 +0300

    props for message

--- mssc-jsm/src/main/java/com/lugonzo/jsm/model/HelloWorldMessage.java
+++ mssc-jsm/src/main/java/com/lugonzo/jsm/model/HelloWorldMessage.java
@@ -1,2 +1,21 @@
-package com.lugonzo.jsm.model;public class HelloWorldMessage {
+package com.lugonzo.jsm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID= -6769420488796968941L;
+
+    private UUID id;
+    private String message;
 }
