d2463dfef8056e3e22717f30bd7704382c302ed3
Abuu001 <abulugz@gmail.com>
2021-07-24 21:34:35 +0300

    configured JPA repository

--- mssc-beer-service/src/main/java/com/lugonzo/msscbeerservice/repository/BeerRepository.java
+++ mssc-beer-service/src/main/java/com/lugonzo/msscbeerservice/repository/BeerRepository.java
@@ -1,11 +1,11 @@
 package com.lugonzo.msscbeerservice.repository;
 
 import com.lugonzo.msscbeerservice.model.BeerDto;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface BeerRepository extends PagingAndSortingRepository<BeerDto, UUID> {
+public interface BeerRepository extends JpaRepository<BeerDto, UUID> {
 }
