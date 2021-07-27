5002742fe0edb9e3dc8b65122741ed39c6b59e69
Abuu001 <abulugz@gmail.com>
2021-07-27 22:28:17 +0300

    md

--- mssc-gateway/src/main/java/com/lugonzo/gateway/config/LocalHostConfig.java
+++ mssc-gateway/src/main/java/com/lugonzo/gateway/config/LocalHostConfig.java
@@ -1,2 +1,20 @@
-package com.lugonzo.gateway.config;public class LocalHostConfig {
+package com.lugonzo.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostConfig {
+
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r->r.path("/api/v1/beer/*")
+                        .uri("http://localhost:3003"))
+                .route(r->r.path("/api/v1/customer/*")
+                        .uri("http://localhost:3002"))
+                .build();
+    }
 }
